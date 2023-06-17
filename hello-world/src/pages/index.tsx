bd1eee5bd60b2211c5d63bd820b984b31107653c
Caro <carole.hug@outlook.com>
2023-06-17 18:32:31 +0200

    create new project

---
+++ hello-world/src/pages/index.tsx
@@ -0,0 +1,18 @@
+import {signOut, useSession} from 'next-auth/react';
+
+export default function Home() {
+    const { data: session } = useSession();
+    const user = session?.user;
+  return (
+      <>
+          <div>
+            <span>{`Hello, you're logged in as ${user?.firstname} ${user?.lastname}!`}</span>
+          </div>
+          <div>
+            <a href="#" onClick={() => signOut({ redirect: true, callbackUrl: '/login' })}>
+              logout
+            </a>
+          </div>
+      </>
+  )
+}
---
+++ hello-world/src/pages/login.tsx
@@ -0,0 +1,23 @@
+import {signIn, signOut, useSession} from 'next-auth/react';
+import {useRouter} from 'next/router';
+import {useEffect} from 'react';
+
+export default function Home() {
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (session) {
+      router.push('/');
+    }
+  });
+
+  return (
+        <div>
+          <button onClick={() => signIn('zitadel')}>
+            Login
+          </button>
+
+    </div>
+  );
+}
